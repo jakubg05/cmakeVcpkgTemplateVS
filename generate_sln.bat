89f546c9b87059832459b4a96bff75bd92e20798
jakubg05 <jakub.gordiak@gmail.com>
2023-12-21 05:55:16 +0100

    not working commit trying core app architecture

--- generate_sln.bat
+++ generate_sln.bat
@@ -1,5 +1,6 @@
 @echo off
-mkdir build
-cd build
-cmake -G "Visual Studio 17 2022" -A x64 ..
+if not exist out mkdir out
+cd out
+cmake -G "Visual Studio 17 2022" -A x64 ../Core
+cmake -G "Visual Studio 17 2022" -A x64 ../App
 cd ..
